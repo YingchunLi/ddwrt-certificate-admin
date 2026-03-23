24d88fd74caa1a685c591f20153ac2d966fbcd38
Yingchun Li <ncepu.lyc@gmail.com>
2026-03-23 22:30:11 +1000

    fix issue with node-fordge upgrade

--- craco.config.js
+++ craco.config.js
@@ -1,13 +1,25 @@
+const webpack = require('webpack');
+
 module.exports = {
   webpack: {
     configure: (webpackConfig) => {
       webpackConfig.resolve = webpackConfig.resolve || {};
       webpackConfig.resolve.fallback = {
         ...(webpackConfig.resolve.fallback || {}),
-        os: require.resolve('os-browserify/browser'),
+        os: require.resolve("os-browserify/browser"),
+        crypto: require.resolve("crypto-browserify"),
+        stream: require.resolve("stream-browserify"),
+        buffer: require.resolve("buffer"),
       };
+
+      webpackConfig.plugins.push(
+        new webpack.ProvidePlugin({
+          process: 'process/browser',
+          Buffer: ['buffer', 'Buffer'],
+        })
+      );
+
       return webpackConfig;
     },
   },
 };
-
